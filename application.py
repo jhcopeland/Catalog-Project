d08e36767c38795d60f7027d874bf2a907d5bcf5
jhcopeland <jhcope02@gmail.com>
2018-09-11 12:58:27 -0500

    Update application.py

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, flash
 app = Flask(__name__)
+app.secret_key = 'secret'
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker

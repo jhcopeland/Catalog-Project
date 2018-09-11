b2295bb584daeedc86c1e45703ee36fa42e0d9e1
jhcopeland <jhcope02@gmail.com>
2018-09-11 12:38:48 -0500

    Update application.py

--- application.py
+++ application.py
@@ -316,5 +316,6 @@ def itemJSON():
 #-------------------------------------------------
 if __name__ == '__main__':
 	app.secret_key = 'secret'
-	app.debug = True
-	app.run(host = '0.0.0.0', port = 5000)
+	#app.debug = True
+	#app.run(host = '0.0.0.0', port = 5000)
+	app.run()

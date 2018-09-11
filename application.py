5aa060b90ba1b531fddde56e2e23253005c80ef5
jhcopeland <jhcope02@gmail.com>
2018-09-11 12:43:16 -0500

    Update application.py

--- application.py
+++ application.py
@@ -316,6 +316,7 @@ def itemJSON():
 #-------------------------------------------------
 if __name__ == '__main__':
 	app.secret_key = 'secret'
+	app.config['SESSION_TYPE'] = 'filesystem'
 	#app.debug = True
 	#app.run(host = '0.0.0.0', port = 5000)
 	app.run()

69402ee741b5b3b93e1acbb331eff4cdac5cfe19
jhcopeland <jhcope02@gmail.com>
2018-09-12 21:36:04 -0500

    Update application.py

--- application.py
+++ application.py
@@ -152,15 +152,16 @@ def getUserID(email):
 # and reset their login_session
 @app.route('/gdisconnect')
 def gdisconnect():
-    access_token = login_session.get('access_token')
+    #access_token = login_session.get('access_token')
     if access_token is None:
+    #if 'username' not in login_session:
         #print 'Access Token is None'
-        response = make_response(json.dumps('Current user not connected.'), 401)
+        response = make_response(json.dumps('Current user (' + login_session['username'] + ') not connected.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
     #print 'In gdisconnect access token is: ', access_token
     #print 'User name is: ', login_session['username']
-    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
+    url = ('https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session.get('access_token'))
     h = httplib2.Http()
     result = h.request(url, 'GET')[0]
     if result['status'] == '200':

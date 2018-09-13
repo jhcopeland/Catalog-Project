da29838e35e43dae7ab840176057657343e2d6ff
jhcopeland <jhcope02@gmail.com>
2018-09-12 22:47:37 -0500

    Update application.py

--- application.py
+++ application.py
@@ -36,7 +36,7 @@ session = DBSession()
 # Store it in login_session for later validation -
 @app.route('/login')
 def showLogin():
-    state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
     login_session['state'] = state
 #    return "The current session state is %s" % login_session['state']
 #    return render_template('login.html')

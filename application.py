08f34adac063db3a5e254a7f1d1d5ab743e85f02
jhcopeland <jhcope02@gmail.com>
2018-09-12 22:28:25 -0500

    Update application.py

--- application.py
+++ application.py
@@ -117,16 +117,16 @@ def gconnect():
         user_id = createUser(login_session)
     login_session['user_id'] = user_id
 
-    output = ''
-    output += '<h1>Welcome, '
-    output += login_session['username']
-    output += '!</h1>'
-    output += '<img src="'
-    output += login_session['picture']
-    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
-    flash("You are now logged in as %s" % login_session['username'])
+    #output = ''
+    #output += '<h1>Welcome, '
+    #output += login_session['username']
+    #output += '!</h1>'
+    #output += '<img src="'
+    #output += login_session['picture']
+    #output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    #flash("You are now logged in as %s" % login_session['username'])
     #print "done!"
-    return output
+    return redirect(url_for('showCatalog'))
 
 # User Helper Functions
 def createUser(login_session):
